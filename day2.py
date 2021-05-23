ca2c88c06b7d422e5abbf234a65db4832a890724
Rakesh0801 <84654476+Rakesh0801@users.noreply.github.com>
2021-05-23 12:50:42 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,10 @@
+employeename="Rakesh"
+Age=22
+designation="Associate Engineer"
+salary=350029.24
+print('my name is',employeename,'iam of',Age,'old')
+print('i works as',designation,'at virtusa and my ctc is',salary)
+
+print('my name is {} iam of {} old' .format(employeename,Age))
+print('i works as {} at virtusa and my ctc is {}'.format(designation,salary))
+print(f'my name is {employeename} iam of {Age} old i works as {designation} at virtusa and my ctc is {salary}')
